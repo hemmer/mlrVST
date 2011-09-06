21836b908c97e8f065d2423bbac7903c0aa51e0b
unknown <Hemmer@.(none)>
2011-09-06 12:24:48 +0100

    Initial commit

---
+++ JuceLibraryCode/JuceHeader.h
@@ -0,0 +1,32 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+    This is the header file that your files should include in order to get all the
+    Juce library headers. You should NOT include juce.h or juce_amalgamated.h directly in
+    your own source files, because that wouldn't pick up the correct Juce configuration
+    options for your app.
+
+*/
+
+#ifndef __APPHEADERFILE_0NRD9LLGO__
+#define __APPHEADERFILE_0NRD9LLGO__
+
+#include "AppConfig.h"
+#if defined (JUCER_VS2010_78A501D)
+ #include "c:/SDKs/juce/juce_amalgamated.h"
+#elif defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../../juce_amalgamated.h"
+#elif defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "../../../../juce_amalgamated.h"
+#endif
+
+namespace ProjectInfo
+{
+    const char* const  projectName    = "mlrVST";
+    const char* const  versionString  = "1.0.0";
+    const int          versionNumber  = 0x10000;
+}
+
+#endif   // __APPHEADERFILE_0NRD9LLGO__
---
+++ JuceLibraryCode/JuceLibraryCode3.cpp
@@ -0,0 +1,21 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+    This file pulls in all the Juce source code, and builds it using the settings
+    defined in AppConfig.h.
+
+    If you want to change the method by which Juce is linked into your app, use the
+    Jucer to change it, rather than trying to edit this file directly.
+
+*/
+
+#include "AppConfig.h"
+#if defined (JUCER_VS2010_78A501D)
+ #include "c:/SDKs/juce/amalgamation/juce_amalgamated3.cpp"
+#elif defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../../amalgamation/juce_amalgamated3.cpp"
+#elif defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "../../../../amalgamation/juce_amalgamated3.cpp"
+#endif
